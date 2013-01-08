7c264fe8164ecebe08dbbf4a8900ec12e37dc17e
Nobuyoshi Nakada <nobu@ruby-lang.org>
2013-01-09 03:45:42 +0900

    generic_erb.rb: new options
    * tool/generic_erb.rb: --show-source and --dump options.

--- tool/generic_erb.rb
+++ tool/generic_erb.rb
@@ -9,17 +9,28 @@ vpath = VPath.new
 timestamp = nil
 output = nil
 ifchange = nil
+dump = nil
 
 opt = OptionParser.new do |o|
   o.on('-t', '--timestamp[=PATH]') {|v| timestamp = v || true}
   o.on('-o', '--output=PATH') {|v| output = v}
   o.on('-c', '--[no-]if-change') {|v| ifchange = v}
+  o.on('-n', '--show-source') {dump = 'src'}
+  o.on('--dump=TYPE', %w[insn src]) {|v| dump = v}
   vpath.def_options(o)
   o.order!(ARGV)
 end
 template = ARGV.shift or abort opt.to_s
 erb = ERB.new(File.read(template), nil, '%')
 erb.filename = template
+case dump
+when 'src'
+  puts erb.src
+  exit
+when 'insn'
+  puts RubyVM::InstructionSequence.compile(erb.src, erb.filename, nil, 0).disasm
+  exit
+end
 result = erb.result
 if output
   if ifchange and (vpath.open(output) {|f| f.read} rescue nil) == result
