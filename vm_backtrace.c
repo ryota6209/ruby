acaaf69fbe47b21a654e99a8b96b09592d1c0ef6
Nobuyoshi Nakada <nobu@ruby-lang.org>
2014-03-16 18:03:34 +0900

    vm_backtrace.c: BacktraceLocation self and class
    * vm_backtrace.c (rb_backtrace_location_struct): add self and
      klass.
    
    * vm_backtrace.c (location_self_m, location_class_m): new methods
      to achieve self and class of the frame.

--- vm_backtrace.c
+++ vm_backtrace.c
@@ -67,6 +67,7 @@ typedef struct rb_backtrace_location_struct {
 	    struct rb_backtrace_location_struct *prev_loc;
 	} cfunc;
     } body;
+    VALUE self, klass;
 } rb_backtrace_location_t;
 
 struct valued_frame_info {
@@ -94,6 +95,8 @@ location_mark_entry(rb_backtrace_location_t *fi)
       default:
 	break;
     }
+    rb_gc_mark(fi->self);
+    rb_gc_mark(fi->klass);
 }
 
 static size_t
@@ -288,6 +291,24 @@ static VALUE
 location_absolute_path_m(VALUE self)
 {
     return location_absolute_path(location_ptr(self));
+}
+
+/*
+ * Returns the self of this frame.
+ */
+static VALUE
+location_self_m(VALUE self)
+{
+    return location_ptr(self)->self;
+}
+
+/*
+ * Returns the class where the method of this frame is defined.
+ */
+static VALUE
+location_class_m(VALUE self)
+{
+    return location_ptr(self)->klass;
 }
 
 static VALUE
@@ -499,6 +520,8 @@ bt_iter_iseq(void *ptr, const rb_control_frame_t *cfp)
     loc->type = LOCATION_TYPE_ISEQ;
     loc->body.iseq.iseq = iseq;
     loc->body.iseq.lineno.pc = pc;
+    loc->self = cfp->self;
+    loc->klass = cfp->klass;
     arg->prev_loc = loc;
 }
 
@@ -510,6 +533,8 @@ bt_iter_cfunc(void *ptr, const rb_control_frame_t *cfp, ID mid)
     loc->type = LOCATION_TYPE_CFUNC;
     loc->body.cfunc.mid = mid;
     loc->body.cfunc.prev_loc = arg->prev_loc;
+    loc->self = cfp->self;
+    loc->klass = cfp->klass;
 }
 
 static VALUE
@@ -1046,6 +1071,8 @@ Init_vm_backtrace(void)
     rb_define_method(rb_cBacktraceLocation, "base_label", location_base_label_m, 0);
     rb_define_method(rb_cBacktraceLocation, "path", location_path_m, 0);
     rb_define_method(rb_cBacktraceLocation, "absolute_path", location_absolute_path_m, 0);
+    rb_define_method(rb_cBacktraceLocation, "self", location_self_m, 0);
+    rb_define_method(rb_cBacktraceLocation, "class", location_class_m, 0);
     rb_define_method(rb_cBacktraceLocation, "to_s", location_to_str_m, 0);
     rb_define_method(rb_cBacktraceLocation, "inspect", location_inspect_m, 0);
 
